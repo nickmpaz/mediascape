0a4c42fa7fa0c3f441cfff2c45b75656bf483be0
Nick Paz <nicholasmpaz@gmail.com>
2019-12-08 21:01:10 -0800

    update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh -i /var/lib/jenkins/.ssh/myAmazonKey.pem ubuntu@ec2-3-135-208-246.us-east-2.compute.amazonaws.com << EOF
+ssh -i ~/secrets/myAmazonKey.pem ubuntu@ec2-3-135-208-246.us-east-2.compute.amazonaws.com << EOF
     cd mediascape
     docker-compose down
     git pull
