267711fdb302474dfa584562575ca7f030ee2726
Nick Paz <nicholasmpaz@gmail.com>
2019-12-08 22:25:53 -0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-ls ~/secrets
-# ssh -i ~/secrets/myAmazonKey.pem ubuntu@ec2-3-135-208-246.us-east-2.compute.amazonaws.com << EOF
-#     cd mediascape
-#     docker-compose down
-#     git pull
-#     docker-compose -f docker-compose.prod.yml up -d --build
-# EOF
+
+ssh -i ~/secrets/myAmazonKey.pem ubuntu@ec2-3-135-208-246.us-east-2.compute.amazonaws.com << EOF
+    cd mediascape
+    ls
+    # docker-compose down
+    # git pull
+    # docker-compose -f docker-compose.prod.yml up -d --build
+EOF
