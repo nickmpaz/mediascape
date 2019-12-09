455a6da6adfad47fe8bdcc343c34bd805039b634
Nick Paz <nicholasmpaz@gmail.com>
2019-12-08 21:19:48 -0800

    nooo

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
-ssh -i ~/secrets/myAmazonKey.pem ubuntu@ec2-3-135-208-246.us-east-2.compute.amazonaws.com << EOF
-    whoami
-EOF
+whoami
+# ssh -i ~/secrets/myAmazonKey.pem ubuntu@ec2-3-135-208-246.us-east-2.compute.amazonaws.com << EOF
+#     whoami
+# EOF
