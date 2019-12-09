c26eccfd6e25345d714f331946e5ad09c020475f
Nick Paz <nicholasmpaz@gmail.com>
2019-12-09 12:59:41 -0800

    first pipeline test

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
 
 ssh -i ~/secrets/myAmazonKey.pem -o StrictHostKeyChecking=no ubuntu@ec2-3-135-208-246.us-east-2.compute.amazonaws.com << EOF
     cd mediascape
-    ls
-    # docker-compose down
-    # git pull
-    # docker-compose -f docker-compose.prod.yml up -d --build
+    docker-compose down
+    git pull
+    docker-compose -f docker-compose.prod.yml up -d --build
 EOF
