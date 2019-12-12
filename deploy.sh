03b364265a09b55cf258096f3fbcd1f35a5e4671
Nick Paz <nicholasmpaz@gmail.com>
2019-12-11 18:34:27 -0800

    remove deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ssh -i ~/secrets/myAmazonKey.pem -o StrictHostKeyChecking=no ubuntu@ec2-3-135-208-246.us-east-2.compute.amazonaws.com << EOF
-    cd mediascape
-    docker-compose down
-    git pull
-    docker-compose -f docker-compose.prod.yml up -d --build
-EOF
