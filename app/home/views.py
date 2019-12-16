ff199baa69050a5d6660cf36ca6c18ef12d9885e
Nick Paz <nicholasmpaz@gmail.com>
2019-12-15 18:35:29 -0800

    added books fixture, homepage & thread edits

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from books.models import Book
 
 
 def index(request):
-    return render(request, 'home/index.html')
+
+    books_list = sorted(
+        Book.objects.all(), key=lambda book: book.comment_set.count(), reverse=True)
+
+    return render(request, 'home/index.html', {
+        'books_list': books_list
+    })
